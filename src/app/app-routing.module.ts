d33f9020378caf3949fb3647dbb00c2ba94edca0
victor dias <victor.dias@meumobi.com>
2018-10-19 11:36:04 +0100

    feat(auth): add guard to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { 
+    path: 'home',
+    canActivate: [AuthGuard],
+    loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
 ];
 
